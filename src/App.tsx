a1c2ae51d9664c2c0a14a21c989a49d26c9ca4bf
zawkindev <zawkindev@gmail.com>
2024-10-12 01:23:16 +0500

    feat: setup prettier, husky and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import './App.css'
-
 function App() {
-
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-green-800 text-4xl">Hello world</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
