6a930b26f3e059fc201b5a50e91dcde56f845f37
zawkindev <zawkindev@gmail.com>
2024-10-20 11:02:31 +0500

    feat: remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -6,7 +5,5 @@ import routes from "./routes";
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
