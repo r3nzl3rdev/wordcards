148d46090e81a07d00cec30796012f6c067e705a
zawkindev <zawkindev@gmail.com>
2024-10-20 22:22:43 +0500

    fix: sticky

--- src/pages/Menu/Irregular.tsx
+++ src/pages/Menu/Irregular.tsx
@@ -58,7 +58,7 @@ const Irregular: React.FC = () => {
             );
           })}
         </div>
-        <div className="w-[98%] lg:w-full max-w-[973px] sticky left-0 flex flex-col justify-center gap-4 ">
+        <div className="w-[98%] lg:w-full max-w-[973px] sticky left-1 flex flex-col justify-center gap-4 ">
           <CommentList commentList={comments} />
           <AddCommentBox />
         </div>
