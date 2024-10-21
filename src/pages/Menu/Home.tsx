ebec9933b7d08528f40f3e867b39165e239e5717
zawkindev <zawkindev@gmail.com>
2024-10-21 09:55:33 +0500

    feat: finish home

--- src/pages/Menu/Home.tsx
+++ src/pages/Menu/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { wordsList } from "../../hardcode/hardcode";
+import { comments, wordsList } from "../../hardcode/hardcode";
+import CommentList from "../../components/CommentList";
+import AddCommentBox from "../../components/AddCommentBox";
 
 const Home: React.FC = () => {
   const alphabet = Array.from({ length: 26 }, (_, i) =>
@@ -35,10 +37,39 @@ const Home: React.FC = () => {
                 <span className="text-lg text-blue-500 hover:text-orange-500">
                   {word}
                 </span>
-                ,
+                {index == wordsList.length - 1 ? "." : ","}
               </Link>
             );
           })}
+        </div>
+
+        <p className="text-2xl font-bold">
+          Namunaviy jumlalari <b className="uppercase">yo'q</b> so'zlar
+        </p>
+        <p>
+          Quyidagi so'zlarda hali misol sifatida keltirilgan iboralar yoki
+          jumlalar mavjud emas. Lekin siz loyihaga yordam berishingiz va ularni
+          qo'shishingiz mumkin! So'z sahifasiga o'ting va "
+          <b>Namuna qo'shish</b>" tugmasini bosing.
+        </p>
+        <div className="flex gap-1 flex-wrap">
+          {wordsList.map((word, index) => {
+            return (
+              <Link key={index} to={`en/${word}`}>
+                <span className="text-lg text-blue-500 hover:text-orange-500">
+                  {word}
+                </span>
+                {index == wordsList.length - 1 ? "." : ","}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="mt-6 lg:mt-12 w-[98%] lg:w-full sticky left-1 flex flex-col justify-center gap-4 ">
+          <p className="text-xl md:text-2xl font-bold">
+            Saytdagi yangi sharhlar
+          </p>
+          <CommentList commentList={comments} />
+          <AddCommentBox />
         </div>
       </div>
     </div>
