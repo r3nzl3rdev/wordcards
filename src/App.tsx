fef6c4dbaacb3e8ec48ab0f8f12c7f8b63b0e29f
zawkindev <zawkindev@gmail.com>
2024-10-22 17:25:36 +0500

    fix: add type for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
-function App() {
+const App: React.FC= ()=> {
   return (
     <>
       <Header />
