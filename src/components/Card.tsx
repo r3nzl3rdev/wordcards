a1d3949a1b76593b9b4b943d3ed81acfed2c628e
zawkindev <zawkindev@gmail.com>
2025-04-15 06:15:10 +0500

    fix: card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Button from "./Button"
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type ButtonColor = 'blue' | 'green'
 
