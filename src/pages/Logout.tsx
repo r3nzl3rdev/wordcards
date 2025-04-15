2fc4b170636adb0a43d2c61ae0a39959e775e511
zawkindev <zawkindev@gmail.com>
2025-04-15 06:14:13 +0500

    fix: user menu

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
 
 const Logout: React.FC = () => {
   useEffect(() => {
     localStorage.clear()
+    window.location.href = '/login'
   }, [])
 
-  return <Navigate to={"/"} />
+  return null
 }
 
 export default Logout
